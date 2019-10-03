045e119d81fff9c2e5c3c4592ad5d37517f35225
Artem Leonov <leonovru@yandex.ru>
2019-10-03 18:01:05 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NumberGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
