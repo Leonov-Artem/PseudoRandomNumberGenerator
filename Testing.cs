798ab03416ea6b9839e9c28f3cd9238737f4c896
Artem Leonov <leonovru@yandex.ru>
2019-10-03 19:29:21 +0300

    Added implementation for class Testing

---
+++ Testing.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NumberGenerator
+{
+    public class Testing
+    {
+        private IRandom random;
+        private int[] frequencies;
+        private int numberIntervals;
+        private int amountNumbers;
+        private int expectedFrequency;
+
+        public Testing(IRandom random, int numberIntervals=20, int amountNumbers=10000)
+        {
+            this.random = random;
+            this.numberIntervals = numberIntervals;
+            this.amountNumbers = amountNumbers;
+
+            frequencies = new int[numberIntervals];
+            expectedFrequency = amountNumbers / numberIntervals;
+        }
+
+        public double ChiSquared()
+        {
+            FilArrayOfFrequencies();
+            double chi = 0;
+
+            for (int i = 0; i < numberIntervals; i++)
+                chi += Math.Pow(frequencies[i] - expectedFrequency, 2) / expectedFrequency;
+
+            return chi;
+        }
+
+        private void FilArrayOfFrequencies()
+        {
+            double[] generatedNumbers = GenerateArrayOfNumbers();
+
+            for (int i = 0; i < amountNumbers; i++)
+            {
+                int hitInterval = CalculateHitInterval(generatedNumbers[i]);
+                frequencies[hitInterval]++;
+            }
+        }
+
+        private double[] GenerateArrayOfNumbers()
+        {
+            var array = new double[amountNumbers];
+            for (int i = 0; i < amountNumbers; i++)
+                array[i] = random.GenerateNumber();
+
+            return array;
+        }
+
+        private int CalculateHitInterval(double num)
+            => (int)(numberIntervals * num);
+    }
+}
